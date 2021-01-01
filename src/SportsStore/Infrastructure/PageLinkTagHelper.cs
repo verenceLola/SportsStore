487237f200afaf47f5aa58116a68df4b305c72a4
Vincent Onyango <verencelola@icloud.com>
2021-01-01 11:12:02 +0300

    - add PageLinkTagHelper class

---
+++ src/SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SportsStore.Models.ViewModels;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace SportsStore.Infrastructure {
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper {
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory helperFactory){
+            urlHelperFactory = helperFactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set;}
+        public PagingInfo PageModel { get; set;}
+        public string PageAction {get; set;}
+
+        public override void Process(TagHelperContext context, TagHelperOutput output){
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            TagBuilder result = new TagBuilder("div");
+            for (int i = 1; i <= PageModel.TotalPages; i++){
+                TagBuilder tag = new TagBuilder("a");
+                tag.Attributes["href"] = urlHelper.Action(PageAction, new {page = i});
+                tag.InnerHtml.Append(i.ToString());
+                result.InnerHtml.AppendHtml(tag);
+            }
+            output.Content.AppendHTML(reult.InnerHtml);
+        }
+    }
+}
